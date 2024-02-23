a9f67c49d2ca7cde8bc19481396cce113bdd71da
emilh_lut <emil.haapalainen@student.lut.fi>
2024-02-23 17:10:37 +0200

    Styling, dashboard

---
+++ dating-app/src/components/ChatInput.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+const ChatInput = () => {
+    const [textarea, setTextarea] = useState(null);
+
+    return (
+        <div className="chat-input">
+            <textarea value={textarea} onChange={(e) => setTextarea(e.target.value)}/>
+            <button className="secondary-button">Send</button>
+        </div>
+    );
+}
+
+export default ChatInput;
--- dating-app/src/pages/Dashboard.js
+++ dating-app/src/pages/Dashboard.js
@@ -1,7 +1,71 @@
+/* React tinder card style
+https://www.npmjs.com/package/react-tinder-card
+https://github.com/3DJakob/react-tinder-card
+Card code snippet: https://github.com/3DJakob/react-tinder-card-demo
+*/
+
+import TinderCard from 'react-tinder-card';
+import { useState } from 'react';
+import ChatContainer from '../components/ChatContainer';
+
 const Dashboard = () => {
+
+    const characters = [
+        {
+          name: 'Richard Hendricks',
+          url: 'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        },
+        {
+          name: 'Erlich Bachman',
+          url: 'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        },
+        {
+          name: 'Monica Hall',
+          url: 'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        },
+        {
+          name: 'Jared Dunn',
+          url: 'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        },
+        {
+          name: 'Dinesh Chugtai',
+          url: 'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        }
+      ]
+      
+    const [LastDirection, setLastDirection] = useState()
+    
+    const swiped = (direction, nameToDelete) => {
+        console.log('removing: ' + nameToDelete)
+        setLastDirection(direction)
+      }
+    
+      const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+      }
+    
     return (
-        <div>
-        <h1>Dashboard</h1>
+        <div className="dashboard">
+            <ChatContainer/>
+            <div className="swipe-container">
+                <div className="card-container">
+                    {characters.map((character) =>
+                        <TinderCard 
+                        className='swipe'
+                        key={character.name}
+                        onSwipe={(dir) => swiped(dir, character.name)}
+                        onCardLeftScreen={() => outOfFrame(character.name)}>
+                            <div style={{ backgroundImage: 'url(' + character.url + ')' }} 
+                            className='card'>
+                                <h3>{character.name}</h3>
+                            </div>
+                        </TinderCard>
+                    )}
+                    <div className="swipe-info">
+                        {LastDirection ? <p>Swiped {LastDirection}</p> : <p/>}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
