51219bac043f477ad5a738d881411779623fad47
emilh_lut <emil.haapalainen@student.lut.fi>
2024-03-02 17:19:40 +0200

    Register handling, cookies, other backend logic

--- dating-app/src/components/AuthModal.js
+++ dating-app/src/components/AuthModal.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const AuthModal = ({ setShowModal, isRegister }) => {
 
@@ -6,18 +9,36 @@ const AuthModal = ({ setShowModal, isRegister }) => {
     const [ password, setPw ] = useState(null);
     const [ checkpw, setCheckPw ] = useState(null);
     const [ error, setError ] = useState(null);
+    const [ cookies, setCookie, removeCookie ] = useCookies(['user']);
+
+    let navigate = useNavigate();
     
 
     const handleClick = () => {
         setShowModal(false);
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             if (isRegister && password !== checkpw) {
                 setError('Passwords do not match');
-            } console.log('TODO: send post request to create account');
+            } else {
+                const response = await axios.post(`http://localhost:8000/${isRegister ? 'register' : 'login'}`, { email, password });
+
+                setCookie("Email", response.data.email);
+                setCookie("UserId", response.data.user_id);
+                setCookie("AuthToken", response.data.token);
+
+                const success = response.status === 201;
+                if (success && isRegister) {
+                    navigate("/welcome");
+                } 
+                if (success && !isRegister) {
+                    navigate("/dashboard");
+                }
+            }
+
         } catch (err) {
             console.log(err);
         }
--- server/index.js
+++ server/index.js
@@ -3,15 +3,57 @@ const PORT = 8000;
 const { MongoClient } = require('mongodb');
 const express = require('express');
 const uri = env.MONGO_URI;
-
+const { v1: uuidv4 } = require('uuid');
 const app = express();
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello!')
 })
 
-app.post('/register', (req, res) => {
-    res.send('Hello!')
+app.post('/register', async (req, res) => {
+    const client = new MongoClient(uri)
+    const { email, password } = req.body
+
+    const genID = uuidv4()
+    const hashPW = await bcrypt.hash(password, 10)
+
+    try {
+        await client.connect()
+        const database = client.db("app-data")
+        const users = database.collection("users")
+
+        const oldUser = await users.findOne({ email })
+        if (oldUser) {
+            res.status(400).send('User already exists.')
+        }
+        
+        const sanitizedEmail = email.toLowerCase()
+        const data = {
+            user_id: genID,
+            email: sanitizedEmail,
+            hashed_password: hashPW
+        }
+        const result = await users.insertOne(data);
+
+        const token = jwt.sign({ user_id: genID }, env.TOKEN_SECRET)
+        res.status(201).json({ token, user_id: genID, email: sanitizedEmail})
+    } catch (e) {
+        console.error(e)
+    }
+
+})
+
+app.post('/login', async (req, res) => {
+    const client = new MongoClient(uri)
+    const { email, password } = req.body
+
+    
 })
 
 app.get('/users', async (req, res) => {
